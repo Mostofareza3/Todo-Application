1fe8dd60f5bcd049c8ca0bef62215ea89c0c4880
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-03-15 11:48:19 +0600

    [Page title component & added scss]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import '@fontsource/poppins';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/GlobalStyles.css';
 
 ReactDOM.render(
   <React.StrictMode>
